18646a13dc193094e2f1b5a805589b3a395ecc93
yanghojoon <yanghojoon8487@gmail.com>
2021-12-05 16:22:05 +0900

    feat: ReminderDetailViewController 클래스 생성 - ReminderRow 열거형 생성

--- Today/ReminderDetailViewController.swift
+++ Today/ReminderDetailViewController.swift
@@ -6,3 +6,25 @@
 //
 
 import UIKit
+
+class ReminderDetailViewController: UITableViewController {
+    enum ReminderRow: Int, CaseIterable {
+        case title
+        case date
+        case time
+        case notes
+        
+        func displayText(for reminder: Reminder?) -> String? {
+            switch self {
+            case .title:
+                return reminder?.title
+            case .date:
+                return reminder?.dueDate.description
+            case .time:
+                return reminder?.dueDate.description
+            case .notes:
+                return reminder?.notes
+            }
+        }
+    }
+}
