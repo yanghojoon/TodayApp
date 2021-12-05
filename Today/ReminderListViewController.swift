c2957bb23a086aa8b9f174f69b78ddae4b1686f0
yanghojoon <yanghojoon8487@gmail.com>
2021-12-05 17:59:27 +0900

    refactor: configure 함수를 활용해 화면에 표시되도록 수정

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -34,12 +34,8 @@ extension ReminderListViewController {
         }
         
         let reminder = Reminder.testData[indexPath.row]
-        let image = reminder.isComplete ? UIImage(systemName: "circle.fill") : UIImage(systemName: "circle")
         
-        cell.titleLabel.text = reminder.title
-        cell.dateLabel.text = reminder.dueDate.description
-        cell.doneButton.setBackgroundImage(image, for: .normal)
-        cell.doneButtonAction = {
+        cell.configure(title: reminder.title, dateText: reminder.dueDate.description, isDone: reminder.isComplete) {
             Reminder.testData[indexPath.row].isComplete.toggle()
             tableview.reloadRows(at: [indexPath], with: .none)
         }
