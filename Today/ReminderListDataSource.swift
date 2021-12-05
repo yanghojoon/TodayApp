efc2c722a41afe698fae2914c4a05ac1874207af
yanghojoon <yanghojoon8487@gmail.com>
2021-12-05 20:45:23 +0900

    feat: 테이블뷰의 dataSource를 위한 extension 생성

--- Today/ReminderListDataSource.swift
+++ Today/ReminderListDataSource.swift
@@ -10,3 +10,26 @@ import UIKit
 class ReminderListDataSource: NSObject {
     
 }
+
+extension ReminderListDataSource: UITableViewDataSource {
+    static let reminderListCellIdentifier = "ReminderListCell"
+    
+    func tableView(_ tableview: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Reminder.testData.count
+    }
+    
+    func tableView(_ tableview: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableview.dequeueReusableCell(withIdentifier: Self.reminderListCellIdentifier, for: indexPath) as? ReminderListCell else {
+            fatalError("Unable to dequeue ReminderCell")
+        }
+        
+        let reminder = Reminder.testData[indexPath.row]
+        
+        cell.configure(title: reminder.title, dateText: reminder.dueDate.description, isDone: reminder.isComplete) {
+            Reminder.testData[indexPath.row].isComplete.toggle()
+            tableview.reloadRows(at: [indexPath], with: .none)
+        }
+        
+        return cell
+    }
+}
--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -20,26 +20,3 @@ class ReminderListViewController: UITableViewController {
         }
     }
 }
-
-extension ReminderListViewController {
-    static let reminderListCellIdentifier = "ReminderListCell"
-    
-    override func tableView(_ tableview: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Reminder.testData.count
-    }
-    
-    override func tableView(_ tableview: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableview.dequeueReusableCell(withIdentifier: Self.reminderListCellIdentifier, for: indexPath) as? ReminderListCell else {
-            fatalError("Unable to dequeue ReminderCell")
-        }
-        
-        let reminder = Reminder.testData[indexPath.row]
-        
-        cell.configure(title: reminder.title, dateText: reminder.dueDate.description, isDone: reminder.isComplete) {
-            Reminder.testData[indexPath.row].isComplete.toggle()
-            tableview.reloadRows(at: [indexPath], with: .none)
-        }
-        
-        return cell
-    }
-}
