e8a9d431291a3ec23ed1668ffb5602e793b720f4
yanghojoon <yanghojoon8487@gmail.com>
2021-12-05 16:16:44 +0900

    feat: tableView의 각 셀에 내용이 나오도록 구현

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -8,6 +8,32 @@
 import UIKit
 
 class ReminderListViewController: UITableViewController {
-
+    
 }
 
+extension ReminderListViewController {
+    static let reminderListCellIdentifier = "ReminderListCell"
+    
+    override func tableView(_ tableview: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Reminder.testData.count
+    }
+    
+    override func tableView(_ tableview: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableview.dequeueReusableCell(withIdentifier: Self.reminderListCellIdentifier, for: indexPath) as? ReminderListCell else {
+            fatalError("Unable to dequeue ReminderCell")
+        }
+        
+        let reminder = Reminder.testData[indexPath.row]
+        let image = reminder.isComplete ? UIImage(systemName: "circle.fill") : UIImage(systemName: "circle")
+        
+        cell.titleLabel.text = reminder.title
+        cell.dateLabel.text = reminder.dueDate.description
+        cell.doneButton.setBackgroundImage(image, for: .normal)
+        cell.doneButtonAction = {
+            Reminder.testData[indexPath.row].isComplete.toggle()
+            tableview.reloadRows(at: [indexPath], with: .none)
+        }
+        
+        return cell
+    }
+}
