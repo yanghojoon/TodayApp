00f815eadc658e6c289606500617c0a99ef9f215
yanghojoon <yanghojoon8487@gmail.com>
2021-12-05 23:16:10 +0900

    feat: title을 받는 configure 함수 구현

--- Today/Views/EditTitleCell.swift
+++ Today/Views/EditTitleCell.swift
@@ -9,4 +9,8 @@ import UIKit
 
 class EditTitleCell: UITableViewCell {
     @IBOutlet var titleTextField: UITextField!
+    
+    func configure(title: String) {
+        titleTextField.text = title
+    }
 }
